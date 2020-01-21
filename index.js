c09a090df2700589717401e26bb8c19f8c5354a7
Jin Kim <jinsungkim1210@gmail.com>
2020-01-21 11:13:37 -0800

    Feat: can play music given YT link

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const ytdl = require("ytdl-core");
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 var version = "1.0.1";
 
+var servers = {};
+
 // For commands
 const PREFIX = "!"
 
 bot.on("ready", () => {
-    console.log("Music queue bot is online and running");
+    console.log("Music queue bot is online and running; Version: " + version);
 });
 
 bot.on("message", message => {
@@ -21,12 +24,42 @@ bot.on("message", message => {
             break;
         // Clears number of messages specified
         case "clear":
-            if (!args[1]) return message.reply("Args not understood");
+            if (!args[1]) return message.reply("Argument not understood");
             message.channel.bulkDelete(args[1]);
             break;
-        // case:
-        //     message.channel.sendMessage("That nigga retarded");
-        //     break;
+        case "play":
+            // For the Connection Function
+            function play(connection, message) {
+                var server = servers[message.guild.id];
+                // Download link to YT video, and plays it
+                // Filter out the video to save bandwidth
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+                server.queue.shift();
+                server.dispatcher.on("end", function() {
+                    // If there is another song to play in the queue
+                    if (server.queue[0]) {
+                        play(connection, message);
+                    } else {
+                        connection.disconnect();
+                    }
+                })
+
+            } 
+            if (!args[1]) return message.reply("You need to follow up with a link");
+            if (!message.member.voiceChannel) return message.reply("You need to be in a valid channel");
+            if (!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            }
+            var server = servers[message.guild.id];
+            // Add song to the queue
+            server.queue.push(args[1]);
+            if (!message.guild.voiceConnection) message.member.join().then(function(connection) {
+                play(connection, message);
+            });
+            break;
+        case "help":
+            message.channel.send("Commands:");
+            message.channel.send("!play [Youtube Link]: Queue a song");
     }
 });
 
